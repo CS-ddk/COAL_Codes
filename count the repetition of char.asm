562592ec15f971636b011603804905142192c816
DAWAR DAWOOD KHAN <salarkhanabc123@gmail.com>
2025-05-21 22:44:26 +0500

    Add files via upload

---
+++ count the repetition of char.asm
@@ -0,0 +1,27 @@
+org 0x0100
+
+mov si, string         
+mov di, counter        
+mov cx, 0              
+
+next_char:
+    lodsb              
+    cmp al, 0          
+    je result
+
+    cmp al, 'l'        
+    jne skip           
+
+    inc cx             
+
+skip:
+    jmp next_char
+
+result:
+    mov al, cl         
+    stosb
+                  
+    ret
+
+string:  db "Hello World", 0
+counter: db 0                     
