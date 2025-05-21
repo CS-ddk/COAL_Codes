d23069643033ebbacbaf5e4587c1b80b6b195453
DAWAR DAWOOD KHAN <salarkhanabc123@gmail.com>
2025-05-21 22:51:43 +0500

    Add files via upload

---
+++ jmp and series number.asm
@@ -0,0 +1,21 @@
+org 100h
+
+xor ax,ax
+xor bx,bx
+
+
+mov ax,1
+mov bx,2
+add ax,bx
+
+jmp mult:
+
+addi:
+add ax,1 
+
+mult:
+mul bx
+jmp addi                  ;1 1 2 3 6 7 14 15 30 31 62 63
+
+
+ret
